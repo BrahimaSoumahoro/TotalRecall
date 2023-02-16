5dadc9438afd96cf6a7417ff662822220e30cfe7
BrahimaSoumahoro <123027055+BrahimaSoumahoro@users.noreply.github.com>
2023-02-16 10:15:49 -0500

    D2_total_recall

---
+++ index.js
@@ -0,0 +1,99 @@
+// Example of What your JS File in VS Code should look like
+
+
+/*
+	Remember!!! Once you’ve completed a problem,
+	COMMENT OUT YOUR WORK before moving on to the next one.
+	This makes it easier to read and debug the current 
+	Solution you are working on.
+*/
+
+
+// I. Variables & Data Types
+// A. Q + A
+   /* 1. We use the "=" operator 
+    2. Also with the "=" operator
+    3. With "=" operator
+    4.  /* Declare: is the fact of choosing then establishing a variable telling the program its identity and nature (what type of element it is)
+            Assigning: is giving the appropriate value to the variable for the purpose of the program, as giving a name to your child
+            Define:is the allocation or granting or putting aside of the memory space / storage for that variable
+
+    5. Pseudocoding is the blueprint of the program. it uses plain human language to describe the procedural structures and steps of the program 
+
+        Pseudocode is used to for to find errors (debugging) the program and also explain the program and its procedural structures and steps to non-programmers. 
+
+// B. Strings
+
+// C. Booleans
+// D. The Farm
+// E. Driver's Ed
+//_____________________________________________________________
+
+
+
+
+
+
+// II. Loops
+// A. The Basics
+// B. Get even
+// C. Give me Five
+// D. Savings account
+//_____________________________________________________________
+
+
+
+
+
+
+
+
+// III. Arrays & Control Flow
+// A. Talk about it:
+// B. Easy Does It
+// C. Accessing elements
+// D. Change values
+// E. Mix It Up
+// F. Biggie Smalls
+// G. Monkey in the Middle
+// H. What's in Your Closet?
+//_____________________________________________________________
+
+
+
+
+
+
+
+
+// IV. Functions
+// A.
+// B.
+// C.
+// D.
+// E.
+// F.
+// G.
+// H.
+//_____________________________________________________________
+
+
+
+
+
+
+// V. Objects
+// A.
+// B.
+// C.
+// D.
+// E.
+// F.
+// G.
+//_____________________________________________________________
+
+
+// Extra
+
+
+
