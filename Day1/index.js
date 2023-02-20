df30aa7838c0dff56d6fdc9f43e09f0f498e5666
BrahimaSoumahoro <123027055+BrahimaSoumahoro@users.noreply.github.com>
2023-02-20 12:05:35 -0500

    Day1

--- Day1/index.js
+++ Day1/index.js
@@ -148,13 +148,7 @@
 
 
 // B. Get even
-        // let i = 1; 
-        // while (i <= 100) {
-        // num = i++;
-        // console.log(" The number is "  +num);
-        // }
-
-
+     
         let i = 1; 
         while (i <= 100) {
         if (i %2 == 0 ) {
