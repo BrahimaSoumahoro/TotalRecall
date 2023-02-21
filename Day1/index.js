79c422f7d38a0e980da2d0609f3d7a0988b2adc6
BrahimaSoumahoro <123027055+BrahimaSoumahoro@users.noreply.github.com>
2023-02-20 22:04:44 -0500

    Day1

--- Day1/index.js
+++ Day1/index.js
@@ -164,19 +164,6 @@
 // console.log(typeof num);
 // C. Give me Five
 
-        // let i = 0; 
-        // while (i <= 100; i++) {
-        // // if (i %5 == 0) {
-        // //         console.log( "I found a " + i + " High Five");
-        //         // console.log(typeof i);
-        //         if (i %3 == 0) {
-        //            console.log("I found a " + i + " Three is a crowd");
-        //         }
-        // } else{
-        //         console.log(i + " Low five for you");
-        // }
-        
-        // } 
 
         for (let i = 1; i <=100; i++) {
                 if (i % 5 === 0 && i % 3 === 0)   {
